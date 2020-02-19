6e851c630677c5b394b57091e81f0dca0ccfb9f0
Timofey Karuba <tvkaruba@outlook.com>
2020-02-19 03:27:49 +0300

    Add skeleton.

---
+++ Art.Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Art.Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
